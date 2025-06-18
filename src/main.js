b9faf69c4e1fab51c3d3efdebaa6e6593e5f754a
bogdanzmeul <bogdanzmeul@gmail.com>
2025-06-18 13:18:59 +0300

    Refactor code

--- src/main.js
+++ src/main.js
@@ -2,18 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleCartButton = document.getElementById('toggleCartButton');
     const cartAside = document.getElementById('cartAside');
 
-    toggleCartButton.addEventListener('click', function() {
-        cartAside.classList.toggle('active');
-        const isExpanded = cartAside.classList.contains('active');
-        toggleCartButton.setAttribute('aria-expanded', isExpanded);
-    });
-
     document.addEventListener('click', function(event) {
         if (cartAside.classList.contains('active') &&
             !cartAside.contains(event.target) &&
             !toggleCartButton.contains(event.target)) {
             cartAside.classList.remove('active');
-            toggleCartButton.setAttribute('aria-expanded', 'false');
+        } else if(toggleCartButton.contains(event.target)) {
+            cartAside.classList.toggle('active');
         }
     });
 });
